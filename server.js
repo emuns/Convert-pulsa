712cc6c001ad6aee308fd651dc615248ddf9952c
emuns <Hermantobnx1@gmail.com>
2026-04-06 01:42:12 +0700

    fix port railway

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ app.get('/logout', (req, res) => {
 })
 
 // ===== START SERVER =====
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Server jalan di port ' + PORT)
 })
