5fe457144f96fe11fbb61419bde689aa5e55ce9c
emuns <73863680+emuns@users.noreply.github.com>
2026-04-06 16:41:44 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,1 @@
-document.getElementById("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  const nominal = document.getElementById("nominal").value;
-  const provider = document.getElementById("provider").value;
-
-  let rate = 0.8;
-
-  if (provider === "telkomsel") rate = 0.85;
-  if (provider === "xl") rate = 0.8;
-  if (provider === "axis") rate = 0.75;
-
-  const hasil = nominal * rate;
-
-  document.getElementById("result").innerHTML =
-    `Hasil convert: Rp ${hasil.toLocaleString()}`;
-});
+console.log("App jalan");
