f60b91a812ba9fde83ff45f62277100f21683cfb
emuns <73863680+emuns@users.noreply.github.com>
2026-04-06 08:48:13 +0700

    Fix root route to send server status
    Removed redundant route for root URL.

--- server.js
+++ server.js
@@ -29,11 +29,8 @@ const ADMIN_PASS = process.env.ADMIN_PASS || "12345"
 
 // ROOT
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'))
-app.get("/", (req, res) => {
   res.send("SERVER HIDUP ✅");
 });
-
 // LOGIN PAGE
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'))
